a92fa243a259bc73ff98651d71982de90bf145f5
Agnihotri, Pawan <Pawan.Agnihotri@vericast.com>
2023-06-15 00:06:00 -0400

    draft

---
+++ SQLAssignment/airlines.sql
@@ -0,0 +1,19 @@
+CREATE TABLE  airlines (
+    "IATA_CODE" VARCHAR2(40),
+    "AIRLINE" VARCHAR2(40)
+);
+INSERT INTO airlines VALUES
+    ('UA','United Air Lines Inc.');
+    INSERT INTO airlines VALUES('AA','American Airlines Inc.');
+    INSERT INTO airlines VALUES('US','US Airways Inc.');
+    INSERT INTO airlines VALUES('F9','Frontier Airlines Inc.');
+    INSERT INTO airlines VALUES('B6','JetBlue Airways');
+    INSERT INTO airlines VALUES('OO','Skywest Airlines Inc.');
+    INSERT INTO airlines VALUES('AS','Alaska Airlines Inc.');
+    INSERT INTO airlines VALUES('NK','Spirit Air Lines');
+    INSERT INTO airlines VALUES('WN','Southwest Airlines Co.');
+    INSERT INTO airlines VALUES('DL','Delta Air Lines Inc.');
+    INSERT INTO airlines VALUES('EV','Atlantic Southeast Airlines');
+    INSERT INTO airlines VALUES('HA','Hawaiian Airlines Inc.');
+    INSERT INTO airlines VALUES('MQ','American Eagle Airlines Inc.');
+    INSERT INTO airlines VALUES('VX','Virgin America');
